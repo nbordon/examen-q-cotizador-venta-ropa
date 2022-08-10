6c2cf4801a6f871c4f39332f614ee286a8d1c0a8
Pedro Nicolas Bordon <nbordon@frba.utn.edu.ar>
2022-08-10 00:37:22 -0300

    feat: resolucion Parte B

--- Modelo/Prenda.cs
+++ Modelo/Prenda.cs
@@ -3,7 +3,30 @@
     public abstract class Prenda
     {
         #region Atributos
-        public abstract double Precio { get; }
+        private CalidadPrenda calidad;
+        private double precioUnitario;
+        private int stock;
         #endregion
+
+        #region Constructores
+        public Prenda(CalidadPrenda calidad, double precio, int stock)
+        {
+            this.calidad = calidad;
+            this.precioUnitario = precio;
+            this.stock = stock;
+        }
+        #endregion
+
+        #region Propiedades
+        public CalidadPrenda Calidad => calidad;
+        public double Precio => precioUnitario;
+        public int Stock => stock;
+        #endregion
+    }
+
+    public enum CalidadPrenda
+    {
+        STANDARD,
+        PREMIUM
     }
 }
---
+++ Modelo/Tienda.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Tienda
+    {
+        #region Atributos
+        private string nombre;
+        private string direccion;
+        private List<Prenda> prendasEnStock;
+        #endregion
+
+        #region Constructores
+        public Tienda(string nombre, string direccion)
+        {
+            this.nombre = nombre;
+            this.direccion = direccion;
+            this.prendasEnStock = new List<Prenda>();
+        }
+        #endregion
+
+        #region Propiedades
+        public string Nombre => nombre;
+        public string Direccion => direccion;
+        public List<Prenda> PrendasEnStock => prendasEnStock;
+        #endregion
+
+        #region Métodos
+        public void AgregarPrendaEnStock(Prenda prenda)
+        {
+            prendasEnStock.Add(prenda);
+        }
+        #endregion
+    }
+}
