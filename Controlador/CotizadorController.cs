1f090ad36c5aa709c95a2e0cb9bc9e8b2e353d7e
Pedro Nicolas Bordon <nbordon@frba.utn.edu.ar>
2022-08-13 21:39:44 -0300

    fix: correcciones en nombres de proyectos y versiones

---
+++ Controlador/CotizadorController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controllador
+{
+    public class CotizadorController
+    {
+        #region Atributos
+        private Modelo.Tienda tienda;
+        private Modelo.Vendedor vendedor;
+        private IViewCotizador _viewCotizador;
+        #endregion
+
+        #region Construcores
+        public CotizadorController(IViewCotizador viewCotizador)
+        {
+            _viewCotizador = viewCotizador;
+        }
+        #endregion
+
+        #region Métodos
+        #endregion
+
+
+    }
+}
---
+++ Controlador/IViewCotizador.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controllador
+{
+    public interface IViewCotizador
+    {
+        #region Métodos
+        double Precio();
+        int Cantidad();
+        bool EsCamisa();
+        bool EsPantalon();
+        bool EsMangaCorta();
+        bool ConCuelloMao();
+        bool EsChupin();
+
+        void MostrarCotizacion(string cotizacion);
+        #endregion
+    }
+}
